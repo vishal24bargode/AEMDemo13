bae878d27e8fff3bb0686f0df44c8f7520bfe5f0
vishalbargode <vishal.bargode@hayagreeva.co.in>
2020-03-10 13:06:06 +0800

    Testing Jenkins Auto Build

--- myproject/src/main/java/myproject/RunMyPackage.java
+++ myproject/src/main/java/myproject/RunMyPackage.java
@@ -5,7 +5,7 @@ public class RunMyPackage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for(int i=1; i<=12; i++) {
+		for(int i=1; i<=15; i++) {
 			System.out.println("Run By Vishal with help of Eclispse "+i);
 		}
 
