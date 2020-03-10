44f0733922b844b620d803321ad012217fc822cf
vishalbargode <vishal.bargode@hayagreeva.co.in>
2020-03-10 14:17:29 +0800

    RunMyPackage[local-March2020]

--- myproject/src/main/java/myproject/RunMyPackage.java
+++ myproject/src/main/java/myproject/RunMyPackage.java
@@ -5,7 +5,7 @@ public class RunMyPackage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for(int i=1; i<=20; i++) {
+		for(int i=1; i<=25; i++) {
 			System.out.println("Run By Vishal with help of Eclispse "+i);
 		}
 
